00eff39533895d83085f33db7098c9f267fb0d3c
Obby Sidane YOUTA <dyoby237@gmail.com>
2024-10-03 11:41:30 +0200

    Fix mobile view

---
+++ resources/views/components/subtitle.blade.php
@@ -0,0 +1,7 @@
+@props([
+  'text' => 'Subtitle',
+])
+
+<p class="text-lg subtitle font-light leading-3 mt-2">
+  {{ $text }}
+</p>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,40 +3,36 @@
 @section('content')
   <section id="section-1" class="mt-[100px]">
     <div class="">
-      <h2 class="uppercase font-bold text-sm text-secondary mb-0">
-        Nisi nec velit
-      </h2>
-      <p class="text-lg subtitle font-light leading-3">
-        Nullam pulvinar felis at metus malesuada
-      </p>
-      <p class="text-md leading-2 font-light">
-        Pellentesque tincidunt tristique neque, eget venenatis enim gravida quis. Fusce at egestas libero. Cras convallis egestas ullamcorper. Suspendisse sed ultricies nisl, pharetra rutrum mauris. Vestibulum at massa dui. Morbi et purus velit. Etiam tristique, justo eu condimentum efficitur, purus velit facilisis sem, id fringilla tortor quam quis dolor. Praesent ultricies dignissim ex, at volutpat sapien ullamcorper rhoncus.
-      </p>
+      <x-title text="Nisi nec velit" />
+      <x-subtitle text="Nullam pulvinar felis at metus malesuada" />
+      <div>
+        <x-section-text>
+          Pellentesque tincidunt tristique neque, eget venenatis enim gravida quis. Fusce at egestas libero. Cras convallis egestas ullamcorper. Suspendisse sed ultricies nisl, pharetra rutrum mauris. Vestibulum at massa dui. Morbi et purus velit. Etiam tristique, justo eu condimentum efficitur, purus velit facilisis sem, id fringilla tortor quam quis dolor. Praesent ultricies dignissim ex, at volutpat sapien ullamcorper rhoncus.
+        </x-section-text>
+      </div>
       <div>
         <x-link />
       </div>
     </div>
     <div class="mt-16">
-      @php
-        $asset = \Roots\asset('images/img.png');
-      @endphp
-      <img src="{{ $asset->uri() }}" alt="image" class="image-387 object-cover" />
+      <div>
+        @php
+          $asset = \Roots\asset('images/img.png');
+        @endphp
+        <img src="{{ $asset->uri() }}" alt="image" class="image-387 object-cover" />
+      </div>
     </div>
   </section>
 
   <section class="mt-[100px]">
     <div>
-      <h2 class="uppercase font-bold text-sm text-secondary mb-0">
-        Bibendum curabitur magna
-      </h2>
-      <p class="text-lg font-light leading-3">
-        Aenean sed nibh a magna posuere
-      </p>
+      <x-title text="Bibendum curabitur magna" />
+      <x-subtitle text="Aenean sed nibh a magna posuere" />
       <div>
-        <p>
+        <x-section-text>
           Maecenas interdum lorem eleifend orci aliquam mollis aliquam non rhoncus magna :
-        </p>
-        <ul class="list-primary">
+        </x-section-text>
+        <ul class="list-primary list-inside text-md leading-2">
           <li>
             Suscipit libero
           </li>
@@ -48,84 +44,93 @@
           </li>
         </ul>
       </div>
-      <div>
+      <div class="mt-5">
         <x-link />
       </div>
     </div>
-    <div>
-      @php
-        $asset = \Roots\asset('images/img-section2.png');
-      @endphp
-      <img src="{{ $asset->uri() }}" alt="image" class="image-387 object-cover" />
+    <div class="mt-16">
+      <div class="bg-section2">
+        @php
+          $asset = \Roots\asset('images/img-section2.png');
+        @endphp
+        <img src="{{ $asset->uri() }}" alt="image" class="image-599 object-cover -ml-2" />
+      </div>
     </div>
   </section>
 
   <section class="mt-[100px]">
     <div>
-      <h2 class="uppercase font-bold text-sm text-secondary mb-0">
-        Massa metus proin
-      </h2>
-      <p class="text-lg font-light leading-3">
-        Dolor imperdiet
-      </p>
-      <p>
-        Phasellus risus turpis, pretium sit amet magna non, molestie ultricies enim. Nullam pulvinar felis at metus malesuada.
-      </p>
+      <x-title text="Massa metus proin" />
+      <x-subtitle text="Dolor imperdiet" />
       <div>
-        <x-link />
+        <x-section-text>
+          Phasellus risus turpis, pretium sit amet magna non, molestie ultricies enim. Nullam pulvinar felis at metus malesuada.
+        </x-section-text>
       </div>
     </div>
     <div>
-      <div>
+      <div class="flex gap-2 w-full overflow-x-auto">
         @foreach(['img-slide1.png', 'img-slide2.png', 'img-slide3.png', 'img-slide4.png', ] as $image)
         <div>
           @php
             $asset = \Roots\asset('images/' . $image);
           @endphp
-          <img src="{{ $asset->uri() }}" alt="image" class="image-387 object-cover" />
+          <img src="{{ $asset->uri() }}" alt="image" class="slide-img object-cover" />
         </div>
         @endforeach
+      </div>
+      <div class="mt-4 flex gap-2">
+        <button type="button" class="slide-button cursor-pointer text-gray opacity-40">
+          <svg id="arrow-right-icon" class="icon reverse-180" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+            <path id="Path_224" data-name="Path 224" d="M25.231,15.176a1.337,1.337,0,0,0-.289-1.453L15.609,4.391a1.333,1.333,0,0,0-1.885,1.885l7.057,7.057H5.333a1.333,1.333,0,1,0,0,2.667H20.781l-7.057,7.057a1.333,1.333,0,1,0,1.885,1.885l9.332-9.332A1.334,1.334,0,0,0,25.231,15.176Z" transform="translate(1.333 1.333)" fill="current"/>
+          </svg>
+        </button>
+        <button type="button" class="slide-button cursor-pointer">
+          <svg id="arrow-right-icon" class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+            <path id="Path_224" data-name="Path 224" d="M25.231,15.176a1.337,1.337,0,0,0-.289-1.453L15.609,4.391a1.333,1.333,0,0,0-1.885,1.885l7.057,7.057H5.333a1.333,1.333,0,1,0,0,2.667H20.781l-7.057,7.057a1.333,1.333,0,1,0,1.885,1.885l9.332-9.332A1.334,1.334,0,0,0,25.231,15.176Z" transform="translate(1.333 1.333)" fill="current"/>
+          </svg>
+        </button>
       </div>
     </div>
   </section>
 
-  <section>
+  <section class="mt-[100px]">
     <div>
-      <h2 class="uppercase text-sm text-secondary">
-        Sodales aliquam
-      </h2>
-      <p>
-        Odio mauris diam
-      </p>
-      <p>
-        Phasellus risus turpis, pretium sit amet magna non, molestie ultricies enim. Nullam pulvinar felis at metus malesuada.
-      </p>
+      <x-title text="Sodales aliquam" />
+      <x-subtitle text="Odio mauris diam" />
+      <div>
+        <x-section-text>
+          Phasellus risus turpis, pretium sit amet magna non, molestie ultricies enim. Nullam pulvinar felis at metus malesuada.
+        </x-section-text>
+      </div>
     </div>
-    <div>
-      <form>
+    <div class="mt-8">
+      <form id="contact-form">
         <x-input name="firstname" placeholder="Dupond" label="Nom" />
         <x-input name="lastname" placeholder="" label="Prénom" />
         <x-input name="email" placeholder="" label="Adresse email" type="email" />
-        <div>
-          <label for="subject" class="">
+        <x-input name="phone" placeholder="" label="Téléphone (facultatif)" type="tel" :required="false" placeholder="+33" />
+
+        <div class="relative flex items-center my-4">
+          <label for="subject" class="absolute top-0 h-full text-gray w-[64px] flex items-center left-3">
             Sujet
           </label>
-          <select id="subject" name="subject" required>
+          <select id="subject" name="subject" class="pl-[72px] w-full" required>
             <option value="" disabled selected>Choisissez un sujet</option>
             <option value="1">Option 1</option>
             <option value="2">Option 2</option>
             <option value="3">Option 3</option>
           </select>
         </div>
-        <div>
-          <label for="message">
+        <div class="relative my-4">
+          <label for="message" class="absolute top-2 h-full text-gray w-[64px] left-3 h-[16px]">
             Message
           </label>
-          <textarea name="message" class=""
+          <textarea name="message" class="pt-[20px] w-full"
                     id="message" required></textarea>
         </div>
 
-        <div class="grid gap-2 p-0">
+        <div class="grid gap-2 p-0 my-4">
           <input type="checkbox" name="checkbox" class="p-0 m-0" required />
           <label for="checkbox" class="col-span-11">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quis risus mi. Ut placerat quam lectus. Curabitur dictum velit non lacus ornare tempor.
